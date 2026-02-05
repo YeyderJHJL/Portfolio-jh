68d7272952a2d7d2c1628d21b1d8bb90192b6a0e
yeyderjhjl <jturpoan.@unsa.edu.pe>
2026-02-05 10:12:53 -0500

    actualización de colores en defaultlayout

--- portfolio-vue/src/layouts/DefaultLayout.vue
+++ portfolio-vue/src/layouts/DefaultLayout.vue
@@ -10,14 +10,14 @@ useDarkMode()
     class="
       min-h-screen
       font-sans
-      text-accent-900
       transition-colors duration-300
 
+      text-text-light-primary
       bg-linear-to-br
-      from-primary-300 via-primary-500 to-primary-700
+      from-primary-500 to-primary-300
 
-      dark:text-accent-100
-      dark:from-primary-950 dark:via-primary-900 dark:to-primary-800
+      dark:text-text-dark-primary
+      dark:from-primary-950 dark:to-primary-800
     "
   >
     <slot />
